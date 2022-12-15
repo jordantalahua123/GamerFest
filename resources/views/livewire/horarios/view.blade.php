311b3bb08f0332fdcdbb66b42f5aa25d2f5be482
Diego Tutin <diegotutin3255@gmail.com>
2022-12-15 15:56:17 -0500

    ACTUALIZACION TABLA HORARIOS

--- resources/views/livewire/horarios/view.blade.php
+++ resources/views/livewire/horarios/view.blade.php
@@ -5,21 +5,15 @@
 			<div class="card">
 				<div class="card-header">
 					<div style="display: flex; justify-content: space-between; align-items: center;">
-						<div class="float-left">
-							<h4><i class="fab fa-laravel text-info"></i>
-							Horario Listing </h4>
-						</div>
-						<div wire:poll.60s>
-							<code><h5>{{ now()->format('H:i:s') }} UTC</h5></code>
-						</div>
+
 						@if (session()->has('message'))
 						<div wire:poll.4s class="btn btn-sm btn-success" style="margin-top:0px; margin-bottom:0px;"> {{ session('message') }} </div>
 						@endif
 						<div>
-							<input wire:model='keyWord' type="text" class="form-control" name="search" id="search" placeholder="Search Horarios">
+							<input wire:model='keyWord' type="text" class="form-control" name="search" id="search" placeholder="Buscar Horarios">
 						</div>
 						<div class="btn btn-sm btn-info" data-toggle="modal" data-target="#createDataModal">
-						<i class="fa fa-plus"></i>  Add Horarios
+						<i class="fa fa-plus"></i>  Agregar Horarios
 						</div>
 					</div>
 				</div>
@@ -32,13 +26,13 @@
 						<thead class="thead">
 							<tr> 
 								<td>#</td> 
-								<th>Videojuegos Id</th>
-								<th>Aulas Id</th>
-								<th>Tiempo Inicio</th>
-								<th>Tiempo Fin</th>
+								<th>VideoJuego</th>
+								<th>Aulas</th>
+								<th>Hora Inicio</th>
+								<th>Hora Final</th>
 								<th>Fecha</th>
 								<th>Observaciones</th>
-								<td>ACTIONS</td>
+								<td>ACCIONES</td>
 							</tr>
 						</thead>
 						<tbody>
@@ -54,11 +48,11 @@
 								<td width="90">
 								<div class="btn-group">
 									<button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Actions
+									Acciones
 									</button>
 									<div class="dropdown-menu dropdown-menu-right">
-									<a data-toggle="modal" data-target="#updateModal" class="dropdown-item" wire:click="edit({{$row->id}})"><i class="fa fa-edit"></i> Edit </a>							 
-									<a class="dropdown-item" onclick="confirm('Confirm Delete Horario id {{$row->id}}? \nDeleted Horarios cannot be recovered!')||event.stopImmediatePropagation()" wire:click="destroy({{$row->id}})"><i class="fa fa-trash"></i> Delete </a>   
+									<a data-toggle="modal" data-target="#updateModal" class="dropdown-item" wire:click="edit({{$row->id}})"><i class="fa fa-edit"></i> Editar </a>							 
+									<a class="dropdown-item" onclick="confirm('Confirm Delete Horario id {{$row->id}}? \nDeleted Horarios cannot be recovered!')||event.stopImmediatePropagation()" wire:click="destroy({{$row->id}})"><i class="fa fa-trash"></i> Eliminar </a>   
 									</div>
 								</div>
 								</td>
