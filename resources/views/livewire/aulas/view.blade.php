2d4115b09e6b90131b60e6ca4139aad84d576c1b
Diego Tutin <diegotutin3255@gmail.com>
2022-12-15 15:33:40 -0500

    Arreglo vista Aulas

--- resources/views/livewire/aulas/view.blade.php
+++ resources/views/livewire/aulas/view.blade.php
@@ -5,21 +5,15 @@
 			<div class="card">
 				<div class="card-header">
 					<div style="display: flex; justify-content: space-between; align-items: center;">
-						<div class="float-left">
-							<h4><i class="fab fa-laravel text-info"></i>
-							Aula Listing </h4>
-						</div>
-						<div wire:poll.60s>
-							<code><h5>{{ now()->format('H:i:s') }} UTC</h5></code>
-						</div>
+
 						@if (session()->has('message'))
 						<div wire:poll.4s class="btn btn-sm btn-success" style="margin-top:0px; margin-bottom:0px;"> {{ session('message') }} </div>
 						@endif
 						<div>
-							<input wire:model='keyWord' type="text" class="form-control" name="search" id="search" placeholder="Search Aulas">
+							<input wire:model='keyWord' type="text" class="form-control" name="search" id="search" placeholder="Buscar Aulas">
 						</div>
 						<div class="btn btn-sm btn-info" data-toggle="modal" data-target="#createDataModal">
-						<i class="fa fa-plus"></i>  Add Aulas
+						<i class="fa fa-plus"></i>  Agregar Aula
 						</div>
 					</div>
 				</div>
@@ -34,9 +28,9 @@
 								<td>#</td> 
 								<th>Nombre</th>
 								<th>Edificio</th>
-								<th>Direccion</th>
+								<th>Dirección</th>
 								<th>Observaciones</th>
-								<td>ACTIONS</td>
+								<td>ACCIONES</td>
 							</tr>
 						</thead>
 						<tbody>
@@ -50,7 +44,7 @@
 								<td width="90">
 								<div class="btn-group">
 									<button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Actions
+									Accciones
 									</button>
 									<div class="dropdown-menu dropdown-menu-right">
 									<a data-toggle="modal" data-target="#updateModal" class="dropdown-item" wire:click="edit({{$row->id}})"><i class="fa fa-edit"></i> Edit </a>							 
