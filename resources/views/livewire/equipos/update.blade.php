ea441a102cedd4865611eefd8b358f6c65af733b
Diego Tutin <diegotutin3255@gmail.com>
2022-12-15 16:14:11 -0500

    Arreglo Views y Update tabla equipo y videojuegos

--- resources/views/livewire/equipos/update.blade.php
+++ resources/views/livewire/equipos/update.blade.php
@@ -1,31 +1,38 @@
 <!-- Modal -->
-<div wire:ignore.self class="modal fade" id="updateModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="updateModalLabel" aria-hidden="true">
+<div wire:ignore.self class="modal fade" id="updateModal" data-backdrop="static" tabindex="-1" role="dialog"
+    aria-labelledby="updateModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-       <div class="modal-content">
+        <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="updateModalLabel">Update Equipo</h5>
+                <h5 class="modal-title" id="updateModalLabel">Actualizar Equipo</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span wire:click.prevent="cancel()" aria-hidden="true">×</span>
                 </button>
             </div>
             <div class="modal-body">
                 <form>
-					<input type="hidden" wire:model="selected_id">
-            <div class="form-group">
-                <label for="nombre"></label>
-                <input wire:model="nombre" type="text" class="form-control" id="nombre" placeholder="Nombre">@error('nombre') <span class="error text-danger">{{ $message }}</span> @enderror
-            </div>
-            <div class="form-group">
-                <label for="Observaciones"></label>
-                <input wire:model="Observaciones" type="text" class="form-control" id="Observaciones" placeholder="Observaciones">@error('Observaciones') <span class="error text-danger">{{ $message }}</span> @enderror
-            </div>
+                    <input type="hidden" wire:model="selected_id">
+                    <div class="form-group">
+                        <label for="nombre"></label>
+                        <input wire:model="nombre" type="text" class="form-control" id="nombre"
+                            placeholder="Nombre">@error('nombre') <span class="error text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="Observaciones"></label>
+                        <input wire:model="Observaciones" type="text" class="form-control" id="Observaciones"
+                            placeholder="Observaciones">@error('Observaciones') <span
+                            class="error text-danger">{{ $message }}</span> @enderror
+                    </div>
 
                 </form>
             </div>
             <div class="modal-footer">
-                <button type="button" wire:click.prevent="cancel()" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" wire:click.prevent="update()" class="btn btn-primary" data-dismiss="modal">Save</button>
+                <button type="button" wire:click.prevent="cancel()" class="btn btn-secondary"
+                    data-dismiss="modal">Cerrar</button>
+                <button type="button" wire:click.prevent="update()" class="btn btn-primary"
+                    data-dismiss="modal">Guardar</button>
             </div>
-       </div>
+        </div>
     </div>
 </div>
--- resources/views/livewire/videojuegos/view.blade.php
+++ resources/views/livewire/videojuegos/view.blade.php
@@ -5,21 +5,16 @@
 			<div class="card">
 				<div class="card-header">
 					<div style="display: flex; justify-content: space-between; align-items: center;">
-						<div class="float-left">
-							<h4><i class="fab fa-laravel text-info"></i>
-							Videojuego Listing </h4>
-						</div>
-						<div wire:poll.60s>
-							<code><h5>{{ now()->format('H:i:s') }} UTC</h5></code>
-						</div>
+
+
 						@if (session()->has('message'))
 						<div wire:poll.4s class="btn btn-sm btn-success" style="margin-top:0px; margin-bottom:0px;"> {{ session('message') }} </div>
 						@endif
 						<div>
-							<input wire:model='keyWord' type="text" class="form-control" name="search" id="search" placeholder="Search Videojuegos">
+							<input wire:model='keyWord' type="text" class="form-control" name="search" id="search" placeholder="Buscar Videojuegos">
 						</div>
 						<div class="btn btn-sm btn-info" data-toggle="modal" data-target="#createDataModal">
-						<i class="fa fa-plus"></i>  Add Videojuegos
+						<i class="fa fa-plus"></i>  Agregar Videojuegos
 						</div>
 					</div>
 				</div>
@@ -32,12 +27,12 @@
 						<thead class="thead">
 							<tr> 
 								<td>#</td> 
-								<th>Categorias Id</th>
+								<th>Categoria</th>
 								<th>Nombre</th>
-								<th>Compania</th>
+								<th>Compañia</th>
 								<th>Precio</th>
-								<th>Descripcion</th>
-								<td>ACTIONS</td>
+								<th>Descripción</th>
+								<td>ACCIONES</td>
 							</tr>
 						</thead>
 						<tbody>
@@ -52,11 +47,11 @@
 								<td width="90">
 								<div class="btn-group">
 									<button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Actions
+									Acciones
 									</button>
 									<div class="dropdown-menu dropdown-menu-right">
-									<a data-toggle="modal" data-target="#updateModal" class="dropdown-item" wire:click="edit({{$row->id}})"><i class="fa fa-edit"></i> Edit </a>							 
-									<a class="dropdown-item" onclick="confirm('Confirm Delete Videojuego id {{$row->id}}? \nDeleted Videojuegos cannot be recovered!')||event.stopImmediatePropagation()" wire:click="destroy({{$row->id}})"><i class="fa fa-trash"></i> Delete </a>   
+									<a data-toggle="modal" data-target="#updateModal" class="dropdown-item" wire:click="edit({{$row->id}})"><i class="fa fa-edit"></i> Editar </a>							 
+									<a class="dropdown-item" onclick="confirm('Confirm Delete Videojuego id {{$row->id}}? \nDeleted Videojuegos cannot be recovered!')||event.stopImmediatePropagation()" wire:click="destroy({{$row->id}})"><i class="fa fa-trash"></i> Eliminar </a>   
 									</div>
 								</div>
 								</td>
