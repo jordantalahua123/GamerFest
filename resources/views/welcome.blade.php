61ad92ea1da5f009ac202a932015302edad1c441
jordantalahua123 <jordantalahua@gmail.com>
2023-01-19 21:39:55 -0500

    Limpiar cache de la pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,10 @@
 
 
     <head>
+        <meta http-equiv="Expires" content="0">
+<meta http-equiv="Last-Modified" content="0">
+<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+<meta http-equiv="Pragma" content="no-cache">
         <script
         defer
         src = "https://app.embed.im/snow.js"></script>
